0fa45a5c2261e6046c037563daca62e582e43de4
emrymcgill <emrymcgill@gmail.com>
2024-03-27 17:39:17 +1300

    first commit

---
+++ frontend/src/components/Post.svelte
@@ -0,0 +1,62 @@
+<script>
+    export let title = ''
+    export let desc = ''
+    export let post = null
+
+    let checked = false
+
+    const handleCheck = () => {
+        checked = !checked
+    }
+</script>
+
+<input class='check' type='checkbox' checked={checked} />
+<div class='container'>
+    <button on:click={handleCheck}>
+        <h2 class='title'>{title}</h2>
+    </button>
+        <p>{desc}</p>
+        <img alt='img' />
+    <div class='post'>
+        {post}
+    </div>
+</div>
+
+<style>
+    .container {
+        padding: .5rem;
+        pointer-events: none;
+    }
+    .container:hover{
+        background-color: rgb(194, 194, 194);
+    }
+    .check {
+        display: none;
+    }
+    button {
+        border: none;
+        background: none;
+        font-family:'Times New Roman', Times, serif;
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        padding: 0;
+        margin: 0;
+        pointer-events: auto;
+    }
+    .title {
+        margin-bottom: .5rem;
+    }
+    button:hover .title {
+        cursor: pointer;
+        text-decoration: underline;
+    }
+    .post {
+        height: 0;
+        overflow: hidden;
+    }
+
+    .check:checked + .container .post {
+        height: fit-content;
+    }
+</style>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import '../app.css'
+    import Post from '../components/Post.svelte';
+</script>
+
+<div class="container">
+    <div class="header">
+        <h1>Emry Mcgill Blog</h1>
+    </div>
+    <div class='post-container'>
+        <Post 
+        title='Mt. Liverpool, New Zealand' 
+        desc='A cool mountain trip in Aspiring National Park'
+        />
+    </div>
+</div>
+
+<style>
+    .container {
+        background-color: white;
+        width: 80%;
+        height: 100vh;
+        padding: 1rem;
+        margin-top: 1rem;
+        box-shadow: -10px 5px 5px blue;
+    }
+    .header {
+        border-bottom: 1px solid grey;
+        padding-bottom: .5rem;
+        margin-bottom: 1rem;
+    }
+</style>
